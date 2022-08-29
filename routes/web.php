08f8695f9b8cbac3ec2e99cd870ed60126ff8b56
WawinyEdwin <wawinyedwin44@gmail.com>
2022-08-29 09:10:03 +0300

    Add static dashboard

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +17,7 @@ use App\Http\Controllers\HomeController;
 
 // static pages -----------------------------------------------------------
 Route::get('/',[HomeController::class, 'index'])->name('home');
+Route::get('/admin',[AdminController::class, 'index'])->name('admin');
 Route::get('/about',[HomeController::class, 'about'])->name('about');
 Route::get('/terms-of-service',[HomeController::class, 'terms'])->name('terms');
 Route::get('/privacy-policy',[HomeController::class, 'privacy'])->name('privacy');
